3852e4f6bbd46ff7b7e6ee41b4f6acbc62a2cbd1
ali <a.hosseini12@outlook.com>
2020-12-24 21:44:00 +0330

    splash screen styles improved and name added to constant

--- src/Screens/Splash/index.jsx
+++ src/Screens/Splash/index.jsx
@@ -4,14 +4,15 @@ import {
   H1Tag,
   LogoImgTag,
   SplashScreenRowTag,
-  SplashScreenContainerTag
-} from "./../../Styles/splashScreenStyles";
+  SplashScreenContainerTag,
+} from "./splashScreenStyles";
+const CALENDAR_NAME = "Awesome Calendar";
 const SplashScreen = () => {
   return (
     <SplashScreenContainerTag>
       <SplashScreenRowTag>
-        <LogoImgTag src={logo} alt="calendar-logo" />
-        <H1Tag>Awesome Calendar</H1Tag>
+        <LogoImgTag src={logo} alt="calendar logo" />
+        <H1Tag>{CALENDAR_NAME}</H1Tag>
       </SplashScreenRowTag>
     </SplashScreenContainerTag>
   );
