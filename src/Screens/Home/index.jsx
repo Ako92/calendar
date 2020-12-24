e3eab2eb39939f43902cf5195ccbc4039309435e
ali <a.hosseini12@outlook.com>
2020-12-24 21:46:36 +0330

    home page containing tabs added

---
+++ src/Screens/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TabGenerator from "../../Components/TabGenerator/TabGenerator";
+import CalendarScreen from "../CalendarScreen";
+import Events from "../Events";
+const CALENDAR_TITLE = "Calendar";
+const EVENTS_TITLE = "Events";
+const Home = () => {
+  const tabData = [
+    {
+      title: CALENDAR_TITLE,
+      extraClassName: "",
+      Component: CalendarScreen,
+    },
+    {
+      title: EVENTS_TITLE,
+      extraClassName: "",
+      Component: Events,
+    },
+  ];
+  return <TabGenerator TabsContent={tabData} />;
+};
+export default Home;
