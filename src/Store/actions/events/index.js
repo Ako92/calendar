e026b0a69f9b8e1b96ce41faee39a270991c79df
ali <a.hosseini12@outlook.com>
2020-12-25 22:12:29 +0330

    edit event added to redux

--- src/Store/actions/events/index.js
+++ src/Store/actions/events/index.js
@@ -1,8 +1,18 @@
-import { SAVE_EVENT_SUCCESSFUL } from "../../reducers/events/constants";
+import {
+  EDIT_EVENT_SUCCESSFUL,
+  SAVE_EVENT_SUCCESSFUL,
+} from "../../reducers/events/constants";
 
 export const addEvent = (event) => {
   return {
     type: SAVE_EVENT_SUCCESSFUL,
+    payload: event,
+  };
+};
+
+export const editEventByIndex = (event) => {
+  return {
+    type: EDIT_EVENT_SUCCESSFUL,
     payload: event,
   };
 };
--- src/Store/reducers/events/index.js
+++ src/Store/reducers/events/index.js
@@ -1,21 +1,43 @@
-import { SAVE_EVENT_ERROR, SAVE_EVENT_SUCCESSFUL } from "./constants";
+import {
+  EDIT_EVENT_SUCCESSFUL,
+  SAVE_EVENT_ERROR,
+  SAVE_EVENT_SUCCESSFUL,
+} from "./constants";
 
 const initState = {
   hasError: false,
-  events: {},
+  events: [],
 };
 export const eventsReducer = (state = initState, action) => {
   switch (action.type) {
     case SAVE_EVENT_SUCCESSFUL: {
+      /**
+       * insert new event into events list
+       */
       return {
         ...state,
-        events: {...state.events, ...action.payload},
+        events: [...state.events, action.payload],
       };
     }
     case SAVE_EVENT_ERROR: {
       return {
         ...state,
         hasError: true,
+      };
+    }
+
+    case EDIT_EVENT_SUCCESSFUL: {
+      /**
+       * find data by index and replace it with new object sent from component
+       */
+      const tempEvents = [
+        ...state.events.slice(0, action.payload.index),
+        action.payload.event,
+        ...state.events.slice(action.payload.index + 1),
+      ];
+      return {
+        ...state,
+        events: tempEvents,
       };
     }
     default:
