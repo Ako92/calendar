e946687014436d64596e4ff4ab585dae341bab8e
ali <a.hosseini12@outlook.com>
2020-12-25 12:37:10 +0330

    icons packs updated

--- src/Screens/Splash/index.jsx
+++ src/Screens/Splash/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./../../logo.png";
 import {
   H1Tag,
   LogoImgTag,
@@ -7,11 +6,12 @@ import {
   SplashScreenContainerTag,
 } from "./splashScreenStyles";
 const CALENDAR_NAME = "Awesome Calendar";
+
 const SplashScreen = () => {
   return (
     <SplashScreenContainerTag>
       <SplashScreenRowTag>
-        <LogoImgTag src={logo} alt="calendar logo" />
+        <LogoImgTag src="/icons/logo.png" alt="calendar logo" />
         <H1Tag>{CALENDAR_NAME}</H1Tag>
       </SplashScreenRowTag>
     </SplashScreenContainerTag>
