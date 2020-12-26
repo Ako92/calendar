0db47797b3619bbcdb3d3a1bea9e9f44e510b93b
ali <a.hosseini12@outlook.com>
2020-12-26 20:10:22 +0330

    modal events component improved, some extra stuff removed from it

--- src/Components/ModalEvents/index.jsx
+++ src/Components/ModalEvents/index.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import {
-  AddButtonTag,
-  AddEventTag,
-  NoEventContainer,
-} from "./modalEventsStyles";
 import AddEventScreen from "../../Screens/AddEventScreen";
+import { addEvent } from "../../Store/actions/events";
+import ModalRenderer from "../Modal";
 import {
-  addEvent,
-  deleteEvent,
-  editEventByIndex,
-} from "../../Store/actions/events";
-import EventCard from "../EventCard";
+  CircularButtonTag,
+  CircularScreenContainerTag,
+  CircularScreenDescriptionTag,
+} from "../../Styles/globalStyles";
+import EventListRenderer from "../EventListRenderer";
 /**
  * Modal Events
  * Will use to get this day events from reducer and render them
- * If there's no event it should be
+ * If there's no event it should shows add new event content
  *
  * @component
  * @example
@@ -28,79 +25,79 @@ import EventCard from "../EventCard";
 const ModalEvents = (props) => {
   const { CurrentDate } = props;
   const [addNewEvent, setAddNewEvent] = useState(false);
-  const dispatch = useDispatch();
   const [eventToEdit, setEventToEdit] = useState();
+  const dispatch = useDispatch();
+
   const { events } = useSelector(
     (state) => ({
       events: state?.eventsReducer?.events,
     }),
     shallowEqual
   );
-  const CurrentDayEvents = events.filter(
+
+  const onCancelAddOrEditEvent = () => {
+    /**
+     * on add or edit cancel
+     */
+    if (eventToEdit) {
+      return setEventToEdit(null);
+    }
+    setAddNewEvent(false);
+  };
+
+  const onAddEventSubmit = (time, title, description) => {
+    /**
+     * on add submit
+     */
+
+    dispatch(addEvent({ currentDate: CurrentDate, time, title, description }));
+    return setAddNewEvent(false);
+  };
+
+  const currentDayEvents = events.filter(
     (event) => event.currentDate.getTime() === CurrentDate.getTime()
   );
-  if (addNewEvent || eventToEdit) {
-    return (
-      <AddEventScreen
-        InitialValues={eventToEdit?.event}
-        OnSubmit={(time, title, description) => {
-          if (eventToEdit) {
-            dispatch(
-              editEventByIndex({
-                event: { currentDate: CurrentDate, time, title, description },
-                index: eventToEdit.index,
-              })
-            );
-            setEventToEdit(null);
-            return setAddNewEvent(false);
-          }
+  const AddEventModalContent = (
+    <AddEventScreen
+      OnSubmit={onAddEventSubmit}
+      OnCancel={onCancelAddOrEditEvent}
+    />
+  );
 
-          dispatch(
-            addEvent({ currentDate: CurrentDate, time, title, description })
-          );
-          return setAddNewEvent(false);
-        }}
-        OnCancel={() => {
-          if (eventToEdit) {
-            return setEventToEdit(null);
-          }
-          setAddNewEvent(false);
-        }}
-      />
-    );
-  }
-  if (CurrentDayEvents && CurrentDayEvents[0]) {
-    return (
-      <>
-        {CurrentDayEvents?.map((event, index) => {
-          return (
-            <EventCard
-              Title={event.title}
-              OnEdit={() => {
-                setEventToEdit({ event, index });
-              }}
-              OnDelete={() => {
-                dispatch(deleteEvent(index));
-              }}
-            />
-          );
-        })}
-      </>
-    );
-  }
+  const pageContent = (() => {
+    /** this function will decide if there's event/events in current date, it shows event card,
+     *  otherwise it will show no event layout, to add new event  */
+    if (currentDayEvents && currentDayEvents[0]) {
+      return <EventListRenderer ItemsToRender={currentDayEvents} />;
+    } else {
+      return (
+        <CircularScreenContainerTag>
+          <CircularButtonTag
+            onClick={() => {
+              setAddNewEvent(true);
+            }}
+          >
+            +
+          </CircularButtonTag>
+          <CircularScreenDescriptionTag>Add Event</CircularScreenDescriptionTag>
+        </CircularScreenContainerTag>
+      );
+    }
+  })();
 
   return (
     <>
-      <NoEventContainer>
-        <AddButtonTag
-          onClick={() => {
-            setAddNewEvent(true);
-          }}
-        >
-          +
-        </AddButtonTag>
-        <AddEventTag>Add Event</AddEventTag>
-      </NoEventContainer>
+      {pageContent}
+      <ModalRenderer
+        ModalProps={{
+          Title: "Events",
+          Content: AddEventModalContent,
+        }}
+        IsOpenModal={addNewEvent}
+        HandleCloseModal={() => {
+          setAddNewEvent(false);
+        }}
+      />
     </>
   );
 };
