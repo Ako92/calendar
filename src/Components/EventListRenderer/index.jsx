011e76ddf3d7e6c2e69cc31984466d8f8550157d
ali <a.hosseini12@outlook.com>
2020-12-27 20:21:54 +0330

    Detail screen added to EventListRenderer

--- src/Components/EventListRenderer/index.jsx
+++ src/Components/EventListRenderer/index.jsx
@@ -12,12 +12,13 @@ import { deleteEventByID, editEventByID } from "../../Store/actions/events";
 import AddEventScreen from "../../Screens/AddEventScreen";
 import SimpleEventMapper from "../EventMappers/SimpleEventMapper";
 import CategorizedEventMapper from "../EventMappers/CategorizedEventMapper";
+import EventDetail from "../EventDetail";
 
 /**
  * Event List
  * This will use to render list of event cards.
  * also it will handle edit and delete of content
-
+ * and it will render event detail screen as well.
  * @component
  * @example
  *
@@ -36,13 +37,15 @@ import CategorizedEventMapper from "../EventMappers/CategorizedEventMapper";
 const EventListRenderer = (props) => {
   const { ItemsToRender, IsCategorizedByDate } = props;
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const [eventToEdit, setEventToEdit] = useState();
-  const [eventIDToDelete, setEventIDToDelete] = useState();
+  const [eventToEdit, setEventToEdit] = useState(null);
+  const [eventIDToDelete, setEventIDToDelete] = useState(null);
+  const [eventToRenderDetail, setEventToRenderDetail] = useState(null);
   const dispatch = useDispatch();
   const handleCloseModal = () => {
     setEventIDToDelete(null);
     setEventToEdit(null);
     setIsOpenModal(false);
+    setEventToRenderDetail(null);
   };
 
   const onEditEventSubmit = (time, title, description) => {
@@ -73,6 +76,25 @@ const EventListRenderer = (props) => {
       return setEventToEdit(null);
     }
   };
+  const onEdit = (event) => {
+    setIsOpenModal(true);
+    setEventToEdit(event);
+  };
+  const onDelete = (id) => {
+    setIsOpenModal(true);
+    setEventIDToDelete(id);
+  };
+  const handleOpenDetail = (event) => {
+    setIsOpenModal(true);
+    setEventToRenderDetail(event);
+  };
+  const RenderDetail = (
+    <EventDetail
+      OnDelete={onDelete}
+      OnEdit={onEdit}
+      Event={eventToRenderDetail}
+    />
+  );
   const EditEventModalContent = (
     <AddEventScreen
       InitialValues={eventToEdit && eventToEdit}
@@ -91,9 +113,10 @@ const EventListRenderer = (props) => {
 
   const ModalPropsBasedOnEvent = (() => {
     /**
-     * modal contents: can be delete or edit.
+     * modal contents: can be delete, edit or detail
      */
     if (eventIDToDelete) {
+      /** Handle modal to render delete event screen  */
       return {
         content: DeleteItemModalContent,
         title: "Delete Event",
@@ -118,20 +141,20 @@ const EventListRenderer = (props) => {
         ],
       };
     } else if (eventToEdit) {
+      /** Handle modal to render edit event screen  */
       return {
         content: EditEventModalContent,
         title: "Edit Event",
       };
+    } else if (eventToRenderDetail) {
+      /** Handle modal to render detail screen  */
+      return {
+        content: RenderDetail,
+        title: "Details",
+      };
     }
   })();
-  const onEdit = (event) => {
-    setIsOpenModal(true);
-    setEventToEdit(event);
-  };
-  const onDelete = (id) => {
-    setIsOpenModal(true);
-    setEventIDToDelete(id);
-  };
+
   return (
     <>
       {IsCategorizedByDate ? (
@@ -139,12 +162,14 @@ const EventListRenderer = (props) => {
           ItemsToRender={ItemsToRender}
           OnDelete={onDelete}
           OnEdit={onEdit}
+          OnEventClick={handleOpenDetail}
         />
       ) : (
         <SimpleEventMapper
           ItemsToRender={ItemsToRender}
           OnDelete={onDelete}
           OnEdit={onEdit}
+          OnEventClick={handleOpenDetail}
         />
       )}
       <ModalRenderer
