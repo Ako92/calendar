18f169b3e316d0adc33a53fed8e03d9b79a8a5f2
ali <a.hosseini12@outlook.com>
2020-12-27 04:31:04 +0330

    SimpleEventMapper component comments added

--- src/Components/EventMappers/SimpleEventMapper.jsx
+++ src/Components/EventMappers/SimpleEventMapper.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import EventCard from "../EventCard";
 import PropTypes from "prop-types";
+
+/**
+ * Component for render events without any categorization
+ * This will use to render list of event cards.
+
+ * @component
+ * @example
+ *
+ * return (
+ *   <SimpleEventMapper
+ *      ItemsToRender={[{
+ *          description: "123123",
+ *          id: 1609004507508,
+ *          time: "12333",
+ *          title: "ALI"
+ *      }]}
+ *      OnEdit={()=> // on edit function}
+ *      OnDelete={()=> // on edit function}
+ * />
+ * )
+ */
 const SimpleEventMapper = (props) => {
   const { ItemsToRender, OnEdit, OnDelete } = props;
 
