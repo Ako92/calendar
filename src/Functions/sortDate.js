d79b577a7c5c49ebb5baf664e91a96cf858f6688
ali <a.hosseini12@outlook.com>
2020-12-28 23:42:43 +0330

    date comprator sort order changed to asc

--- src/Functions/sortDate.js
+++ src/Functions/sortDate.js
@@ -1,6 +1,6 @@
 export function sortDateComparator(a, b) {
     var dateA = a.currentDate.getTime();
     var dateB = b.currentDate.getTime();
-    return dateA < dateB ? 1 : -1;
+    return dateA > dateB ? 1 : -1;
   }
   
