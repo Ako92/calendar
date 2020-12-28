f42a40b448547518a48793fb9399c63cb58f225a
ali <a.hosseini12@outlook.com>
2020-12-28 19:49:56 +0330

    input styles improved

--- src/Components/Input/inputStyles.jsx
+++ src/Components/Input/inputStyles.jsx
@@ -3,8 +3,11 @@ import { LIGHT_GRAY_COLOR, OCEAN_BLUE_COLOR } from "../../Styles/globalStyles";
 
 export const InputTag = styled.input`
   border: unset;
-  border-bottom: 2px solid ${OCEAN_BLUE_COLOR};
+  border-bottom: 3px solid ${OCEAN_BLUE_COLOR};
   padding: 10px 0;
+  margin: 15px 0;
+  color: ${LIGHT_GRAY_COLOR};
+  font-size: 20px;
   ::placeholder {
     color: ${LIGHT_GRAY_COLOR};
     font-size: 20px;
--- src/Components/TextArea/textAreaStyles.jsx
+++ src/Components/TextArea/textAreaStyles.jsx
@@ -3,9 +3,12 @@ import { LIGHT_GRAY_COLOR, OCEAN_BLUE_COLOR } from "../../Styles/globalStyles";
 
 export const TextAreaTag = styled.textarea`
   border: unset;
-  border-bottom: 2px solid ${OCEAN_BLUE_COLOR};
+  border-bottom: 3px solid ${OCEAN_BLUE_COLOR};
   margin: 15px 0;
+  padding: 10px 0;
   height:250px;
+  color: ${LIGHT_GRAY_COLOR};
+  font-size: 20px;
   ::placeholder {
     color: ${LIGHT_GRAY_COLOR};
     font-size: 20px;
