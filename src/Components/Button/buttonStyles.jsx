262b8a8eab784d49a18413f2b8784a493f956b41
ali <a.hosseini12@outlook.com>
2020-12-28 19:42:48 +0330

    button styles improved

--- src/Components/Button/buttonStyles.jsx
+++ src/Components/Button/buttonStyles.jsx
@@ -5,9 +5,9 @@ export const ButtonTag = styled.button`
   background: ${(props) =>
     props?.Variant === "blue" ? OCEAN_BLUE_COLOR : "#fff"};
   color: ${(props) => (props?.Variant === "blue" ? "#fff" : OCEAN_BLUE_COLOR)};
-  font-size: 12px;
+  font-size: 20px;
   margin: 10px;
   padding: 10px;
-  border: 1px solid ${OCEAN_BLUE_COLOR};
+  border: 2px solid ${OCEAN_BLUE_COLOR};
   width:49%;
 `;
